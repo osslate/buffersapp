2603ad3513f8986b7bc459f6bcea8e9883a1ef9b
Fionn Kelleher <me@fionn.co>
2014-01-18 15:55:05 +0000

    Make HTML/CSS more semantic; start frontend using Angular.

--- js/buffers.js
+++ js/buffers.js
@@ -3,3 +3,17 @@
     Copyright (c) 2013 Fionn Kelleher. All rights reserved.
     Licensed under the BSD 2-Clause License (FreeBSD) - see LICENSE.md.
 */
+
+var moment = require("moment"),
+    fs = require("fs");
+    
+var Buffers = angular.module("Buffers", []);
+
+Buffers.controller("BuffersController", ["$scope", function($scope) {
+    // update time at the right of the input box.
+    $scope.currentTime = moment().format("HH:mm");
+    setInterval(function() {
+        $scope.currentTime = moment().format("HH:mm");
+        $scope.$apply();
+    }, 1000);
+}]);
